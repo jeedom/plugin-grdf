036b996f286dc31736212effe0cc3c692a54854c
jpty <1798636+jpty@users.noreply.github.com>
2024-03-21 17:57:29 +0100

    Limit start date to 3 years
    Informative data are limited to 3 years

--- core/class/grdf.class.php
+++ core/class/grdf.class.php
@@ -178,6 +178,9 @@ class grdf extends eqLogic {
       'start' => (empty($_lastRead)) ? $this->getConfiguration('access_rights')['perim_donnees_' . $direction['short'] . '_debut'] : date('Y-01-01', strtotime($_lastRead)),
       'end' => date('Y-m-d')
     );
+    $start3Y = strtotime("-3 years midnight");
+    if(strtotime($dates['start']) < $start3Y)
+      $dates['start'] = date('Y-m-d',$start3Y);
 
     if ($this->controlAccessRight('perim_donnees_' . $explodeUrl[2], $direction['short'], $dates)) {
       $formatedData = array();
